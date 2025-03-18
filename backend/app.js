82ea00e1a75644afbe8bac6bbd40b109dc617a50
unknown <10288694@student.walsallcollege.ac.uk>
2025-03-18 16:02:03 +0000

    added get to test and i can see the users now

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const userSchema = new mongoose.Schema({
+    _id: String,
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
 mongoose
-    .connect('mongodb://localhost:27017/chatApp', {
+    .connect('mongodb://localhost:27017/chat', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.error('MongoDB connection error:', error));
+
+
+//=============================================================== CREATE 2
+
+//=============================================================== GET 1
+
+app.get('/users', async(req, res)=>{
+    try{
+        const users = await User.find({})
+        res.status(200).json(users)
+    } catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+//=============================================================== UPDATE 3
+
+//=============================================================== DELETE 4
+
+//===============================================================
+
+
+
+app.listen(3307, () => console.log('server is running on port 3307'))
