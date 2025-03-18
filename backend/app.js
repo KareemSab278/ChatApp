76e05ce54af0c29bcdc4caeec929062ac1ed558e
unknown <10288694@student.walsallcollege.ac.uk>
2025-03-18 21:23:59 +0000

    get works fully with the backend. refer to in-code comments for explanations

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,10 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const cors = require('cors');
 const app = express();
-
-//====================================================================================
-
 app.use(cors());
 app.use(express.json());
 
-const userSchema = new mongoose.Schema({
-    _id: String,
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-const User = mongoose.model('User', userSchema);
-
+//====================================================================================
 
 mongoose
     .connect('mongodb://localhost:27017/chat', {
@@ -26,19 +16,98 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.error('MongoDB connection error:', error));
 
+//====================================================================================
 
-//=============================================================== CREATE 2
+const userSchema = new mongoose.Schema({
+    _id: String,
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
 
-//=============================================================== GET 1
+const chatSchema = new mongoose.Schema({
+    _id: String,
+    participants: {type: Array},
+    createdAt: { type: Date, default: Date.now },
+    lastMessageAt : {type: Date, default: Date.now}
+});
 
-app.get('/users', async(req, res)=>{
+const mssgSchema = new mongoose.Schema({
+    _id: String,
+    chatId: String,
+    senderId: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+    isRead: { type: Boolean, default: false }
+});
+
+const User = mongoose.model('User', userSchema);
+const Chat = mongoose.model('chats', chatSchema);
+const Message = mongoose.model('messages', mssgSchema);
+
+//==================================================================================== ENDPOINTS
+
+
+//=============================================================== GET
+
+app.get('/users', async(req, res)=>{    //this is for getting all users. (will be searching users through frontend)
     try{
         const users = await User.find({})
         res.status(200).json(users)
+        console.log(users.map(user => user._id.toString()));
     } catch(error){
         res.status(400).json({message: error.message})
     }
+});
+
+
+app.get('/chats', async (req, res)=>{   //this is to get all chats
+    try{
+        const chats = await Chat.find({})
+        res.status(200).json(chats)
+        console.log(chats.map(chat => chat._id.toString()));
+    } catch (e) {
+        res.status(400).json({message: e.message})
+    }
+});
+
+app.get('/chats/:participant', async (req, res) => {    //users can see which chats theyre participants of (otherwise theyll end up seeing every chat in the app across multiple accounts)
+    try {
+        const { participant } = req.params;
+        const chats = await Chat.find({ participants: participant });
+        if (!chats.length) {
+            console.log('chat doesn’t exist');
+            return res.status(404).json({ message: 'No chats found' });
+        }
+        res.status(200).json(chats);
+        console.log(chats.map(chat => chat._id.toString()));
+    } catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+});
+
+
+
+app.get('/messages', async(req, res)=>{     //get all messages (this is for filtering bad words in conversations so i can ban users)
+    try{
+        const messages = await Message.find({})
+        res.status(200).json(messages)
+    } catch(e){
+        res.status(400).json({message: e.message})
+    }
 })
+
+app.get(`/messages/:_id`, async(req, res)=>{    // get messages by chat
+    try{
+        const{_id} = req.params;
+        const messages = await Message.find({ chatId: _id });
+        res.status(200).json(messages)
+    } catch(e){
+        res.status(400).json({message: e.message})
+    }
+})
+
+
+//=============================================================== CREATE 2
 
 //=============================================================== UPDATE 3
 
