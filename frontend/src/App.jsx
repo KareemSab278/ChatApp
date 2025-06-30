d46c8ee29397743cacdfd362a26c5b3e6a2ba488
Kareem Elsabrouty <10288694@student.walsallcollege.ac.uk>
2025-06-30 18:36:38 +0100

    added animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,40 @@ import ChatPage from "./pages/ChatPage";
 import NavBar from "./components/NavBar";
 import SignUp from "./pages/SignUp";
 import "./styles/App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import { motion, AnimatePresence } from 'framer-motion'
+
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -40 }}
+        transition={{ duration: 0.6 }}
+        style={{ minHeight: '100vh' }}
+      >
+        <NavBar />
+        <Routes location={location}>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/chats" element={<HomePage />} />
+          <Route path="/Chats/:chatId" element={<ChatPage />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
-    <>
     <Router>
-    <NavBar />
-      <Routes>
-        <Route path = "/" element = {<SignIn/>}/>
-        <Route path = "/sign-up" element = {<SignUp/>}/>
-        <Route path = "/chats" element = {<HomePage/>}/>
-        <Route path="/Chats/:chatId" element={<ChatPage />}/> 
-        </Routes>
-      </Router>
-    </>
+      <AnimatedRoutes />
+    </Router>
   );
 }
 
